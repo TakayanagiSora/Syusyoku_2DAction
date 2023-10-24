d7fd949e7dd66e1abaf0c39051968cb8dc0c9b5a
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2023-10-24 15:03:14 +0900

    追加 >> プレイヤー挙動

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,38 +7,6 @@ public class PlayerManager : MonoBehaviour
 {
     private Transform _transform = default;
 
-    private PlayerMove _playerMove = default;
     private PlayerStatus _playerStatus = default;
     private WeaponBase _equippedWeapon = default;
-
-    private GameInputs _gameInputs = default;
-
-    private void OnEnable()
-    {
-        _gameInputs?.Enable();
-    }
-
-    private void Awake()
-    {
-        _transform = this.transform;
-
-        _playerMove = new();
-
-        _gameInputs = new();
-        _gameInputs.Enable();
-
-        _gameInputs.Player.Move.performed += _playerMove.OnMove;
-        _gameInputs.Player.Move.canceled += _playerMove.OnStop;
-        //_gameInputs.Player.
-    }
-
-    private void OnDisable()
-    {
-        _gameInputs?.Disable();
-    }
-
-    private void Update()
-    {
-        _playerMove.Run(_transform);
-    }
 }
