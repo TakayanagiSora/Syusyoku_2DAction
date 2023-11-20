889b42eaf7f366d03007dbc2ee443be7de87ffa2
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2023-11-20 19:57:39 +0900

    追加 >> プールのアクセスシステム

---
+++ Assets/Scripts/System/PoolUser.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public abstract class PoolUser : MonoBehaviour
+{
+    [SerializeField]
+    private List<UsePoolObject> _usePoolObjectNames = new List<UsePoolObject>();
+    protected List<UsePoolObject> UsePoolObjectNames => _usePoolObjectNames;
+}
+
+[Serializable]
+public class UsePoolObject
+{
+    [SerializeField]
+    private string _name = default;
+    [SerializeField]
+    private int _arrayIndex = default;
+    public string Name => _name;
+}
